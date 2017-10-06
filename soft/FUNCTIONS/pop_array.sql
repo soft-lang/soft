d3e3f2e74304f173253ebcae5d54c02fac8a45a3
Joel Jacobson <joel@trustly.com>
2017-10-07 01:35:24 +0200

    W.I.P

--- soft/FUNCTIONS/pop_array.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION Pop_Walkable(boolean[])
-RETURNS boolean[]
-LANGUAGE sql
-AS $$
-SELECT $1[2:array_length($1,1)]
-$$;
--- soft/FUNCTIONS/quote.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION Quote(text)
-RETURNS text
-LANGUAGE sql
-AS $BODY$
-SELECT CASE WHEN $1 LIKE $$%'%$$ THEN pg_catalog.quote_literal($1) ELSE $$'$$||$1||$$'$$ END
-$BODY$;
