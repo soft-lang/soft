b51f85b2f98e792627fa01be8f537c6705c4c2e9
Joel Jacobson <joel@trustly.com>
2017-10-13 15:17:45 +0200

    W.I.P

--- soft/FUNCTIONS/precedence.sql
+++ soft/FUNCTIONS/precedence.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION Precedence(_NodeTypeID integer)
 RETURNS integer
 LANGUAGE sql
+STABLE
 AS $$
 SELECT COALESCE(
     (SELECT MIN(NodeTypeID) FROM NodeTypes WHERE Precedence = (SELECT Precedence FROM NodeTypes WHERE NodeTypeID = $1)),
